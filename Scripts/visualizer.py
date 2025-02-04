f1c6c64b299b3086292b087dd3617cf986c49232
ahmed-lamidi <ahmedlamidi@usf.edu>
2025-02-04 11:01:57 -0500

    fixed visualizer

--- Scripts/visualizer.py
+++ Scripts/visualizer.py
@@ -1,18 +1,19 @@
 #python script to visualize the heart beat data
 # assuming it is called with the correct data
-
-def estimate_spo2(ir_data, ir_length, r_data, ts_arr):
+import time
+import matplotlib.pyplot as plt
+def estimate_spo2(ir_data, ir_length):
     mean_ir_data = sum(ir_data) / ir_length
 
     # we remove the mean part and also invert the signal
     ac_ir_data = [-1 * (data_point - mean_ir_data) for data_point in ir_data]
 
-
     # we are doing a four point moving average now
     for ind in range(len(ac_ir_data) - 3):
-        ac_ir_data[ind] = (ac_ir_data[ind] + ac_ir_data[ind + 1] + ac_ir_data[ind + 2] + ac_ir_data[ind + 3])
+        ac_ir_data[ind] = (ac_ir_data[ind] + ac_ir_data[ind + 1] + ac_ir_data[ind + 2] + ac_ir_data[ind + 3]) / 4
 
     
+    smooth = [-1 * data for data in ac_ir_data]
     #now find the average for the ac portion and use that as the minimum peak height
 
     minimum_peak_height = sum(ac_ir_data) / len(ac_ir_data)
@@ -34,11 +35,14 @@ def estimate_spo2(ir_data, ir_length, r_data, ts_arr):
     #    for index in range(len(valley_indexes) - 1):
     #         peak_interval_sum += timestamp[valley_indexes[index + 1]] - timestamp[valley_indexes[index]]
 
-        average_interval_sum = (peak_interval_sum / len(valley_indexes) - 1)
-        heart_rate = (25 * 60) / peak_interval_sum  # sampling rate * 60 
+        average_interval_sum = (peak_interval_sum / (len(valley_indexes) - 1))
+        heart_rate = (25 * 60) / average_interval_sum  # sampling rate * 60 
         # essentailly saying how many average heart beats can we fit if we take 25 points per second 
         # each heartbeat on average takes average interval sum
         # that is assuming uniform sampling frequency
+        print(heart_rate)
+        print(average_interval_sum)
+        return heart_rate, smooth
 
 
 
@@ -54,7 +58,8 @@ def find_peak_above(peak_locations, count_peaks, data_array,array_len ,min_heigh
     current_ind = 1
     width = 0
     while current_ind < len(data_array):
-        if data_array[current_ind] > min_height_peaks and data_array[current_ind] > data_array[current_ind - 1]:
+        if data_array[current_ind] > data_array[current_ind - 1]:
+            #removed the min part
             #find left edge of the peak 
             # then also make sure we are greater than the minimum height
             width = 1
@@ -62,7 +67,7 @@ def find_peak_above(peak_locations, count_peaks, data_array,array_len ,min_heigh
                 width += 1
             # we get the flat topped peaks
 
-            if (current_ind + width < len(data_array) or data_array[current_ind] > data_array[current_ind + width]) and count_peaks < 15:
+            if (current_ind + width < len(data_array) and data_array[current_ind] > data_array[current_ind + width]) and count_peaks < 15:
                 # check that we are greater than the right side also
                 peak_locations.append(current_ind)
                 count_peaks += 1
@@ -77,7 +82,7 @@ def find_peak_above(peak_locations, count_peaks, data_array,array_len ,min_heigh
 
 
 
-def remove_close_peaks(peakLocations, number_peaks, data_array, min_ind_dist_peaks):
+def remove_close_peaks(peak_locations, number_peaks, data_array, min_ind_dist_peaks):
     # so first we make the locations sorted based on the size of the peaks they contain
 
     # ---------- Old Thinking -----------
@@ -107,24 +112,25 @@ def remove_close_peaks(peakLocations, number_peaks, data_array, min_ind_dist_pea
     # I was wrong in my initial thinking and it basically slowly removes the peaks that are too close
     # still need to change the part of the code with the I
 
-
-    sort_indices_descend(data_array, peakLocations, len(peakLocations))
+    print(peak_locations , "original")
+    sort_indices_descend(data_array, peak_locations)
     index = 0
-    while index < len(peakLocations):
-        temp_array = peakLocations[:index + 1] # keep what is on the left side
-        for innerIndex in range(index + 1, len(peakLocations)):
-            if abs(peakLocations[innerIndex] - peakLocations[index]) > min_ind_dist_peaks:
-                temp_array.append(peakLocations[innerIndex])
-        peakLocations = temp_array
+    while index < len(peak_locations):
+        temp_array = peak_locations[:index + 1]  # Keep peaks on the left
+        for inner_index in range(index + 1, len(peak_locations)):
+            if abs(peak_locations[inner_index] - peak_locations[index]) > min_ind_dist_peaks:
+                temp_array.append(peak_locations[inner_index])
+        peak_locations = temp_array  # Update the list
+        index += 1
 
-    peakLocations.sort()
+    # Restore indices to ascending order
+    peak_locations.sort()
+    print(peak_locations, "locations_sorted")
+    return peak_locations
 
-    
-    return peakLocations
 
-
-def sort_indices_descend(data_array, location_peaks, number_peaks):
-    location_peaks.sort(key=lambda ind: data_array[ind], reversed=True)
+def sort_indices_descend(data_array, location_peaks):
+    location_peaks.sort(key=lambda ind: data_array[ind], reverse=True)
     # sort in descending order based on the data array values
 
 
@@ -133,3 +139,34 @@ def sort_ascending(array):
     # just to keep a similar structure
 
 
+def main():
+    Data_Collection = []
+    with open("Data.csv", "r") as data_file:
+        current_data = []
+        count = 0
+        for row in data_file:
+            row = row.split(";")
+            if row[2] == "":
+                pass
+            elif row[2] != "PPG_R":
+                count += 1
+                current_data.append(int(row[2]))
+                if count == 100:
+                    count = 0
+                    Data_Collection.append(current_data)
+                    current_data = []
+        Data_Collection.append(current_data)
+        for i in Data_Collection:
+            if len(i) != 0:
+                estimate_spo2(i, len(i))
+        indexes = list(range(len(Data_Collection[1])))
+        fig, axes = plt.subplots(2, 1)
+        axes[0].plot(indexes, Data_Collection[1])
+        heart_rate, smooth  = estimate_spo2(Data_Collection[1], len(Data_Collection[1]))
+        axes[1].plot(indexes, smooth)
+        
+        # plt.plot(indexes,markevery=estimate_spo2(Data_Collection[1], len(Data_Collection[1])), ls="", marker="o", label="points")
+
+        plt.show()
+
+main()
