fc1d6db9aafb2ba57bf1cdbf384d07f18f824ef6
ahmed-lamidi <ahmedlamidi@usf.edu>
2025-04-08 16:28:58 -0400

    Change Mac Address

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@
 // constexpr uint16_t THINGSBOARD_PORT = 1883U;
 
 // REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0x7c, 0xdf, 0xa1, 0xfb, 0x2c, 0x30};
+uint8_t broadcastAddress[] = {0x34, 0xcd, 0xb0, 0x06, 0x67, 0x60};
 
 
 WiFiClient espClient; // create a wificlient
