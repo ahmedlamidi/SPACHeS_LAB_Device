79b0dfb8c2fa4c433f3576801416caff8ff80a2e
ahmed-lamidi <ahmedlamidi@usf.edu>
2025-04-17 16:47:36 -0400

    New Mac Address

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,8 @@
 // constexpr uint16_t THINGSBOARD_PORT = 1883U;
 
 // REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0x34, 0xCD, 0xB0, 0x06, 0x67, 0x60};
+uint8_t broadcastAddress[] = {0x34, 0xCD, 0xB0, 0x08, 0x68, 0xA8};
+// 34:CD:B0:08:68:A8
 
 
 
